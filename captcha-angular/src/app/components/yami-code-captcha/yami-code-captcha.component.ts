e9f26f75615f093c4ec4ba065cc47f6c1b2da594
Yami Code <yamicodeofficial@gmail.com>
2019-01-28 18:43:37 +0100

    first commit

---
+++ captcha-angular/src/app/components/yami-code-captcha/yami-code-captcha.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-yami-code-captcha',
+  templateUrl: './yami-code-captcha.component.html',
+  styleUrls: ['./yami-code-captcha.component.css']
+})
+export class YamiCodeCaptchaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ captcha/src/main/java/com/yamicode/captcha/CaptchaApplication.java
@@ -0,0 +1,14 @@
+package com.yamicode.captcha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaptchaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaptchaApplication.class, args);
+	}
+
+}
+
